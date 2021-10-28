4dddcabe54fb726fd4db7a340c9d21be08399fe5
sshaughnessy <s.shaughnessy.311@gmail.com>
2021-10-27 21:38:07 -0500

    progress, but marker colors isn't working

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1Ijoic3NoYXVnaG5lc3N5IiwiYSI6ImNrdXJnMmRzcTU0Z3MycW56aXplN282eWEifQ.-zH8ex8OPFeod0PxllLzlA";
---
+++ static/js/logic.js
@@ -0,0 +1,74 @@
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// initialize map and add satellite layer
+var myMap = L.map("map", {
+    center: [45.52, -122.67],
+    zoom: 4
+});
+
+ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+     attribution:"© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>", 
+     tileSize: 512,
+     maxZoom: 18,
+     zoomOffset: -1,
+     id: "mapbox/satellite-v9",
+     accessToken: API_KEY
+ }).addTo(myMap);
+
+ // read in json and add earthquake markers to the map
+d3.json(url).then(function(data) {
+    // create marker style for L.geoJSON
+    function geojsonStyle(feature) {
+        return { 
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: colorScale(feature.geometry.coordinates[2]),
+            color: "#000000",
+            radius: radiusSize(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
+    
+    // create color scale function
+    function colorScale(depth) {
+        switch(depth) {
+            case depth > -10 && depth <=10:
+            return "#00ff00";
+            case depth > 10 && depth <=30:
+                return "#ccff66";
+            case depth > 30 && depth<=50:
+                return "#ffcc00";
+            case depth > 50 && depth<=70:
+                return "#ff9900";
+            case depth > 70 && depth<=90:
+                return "#ff6633";
+            case depth > 90:
+                return "#ff3300";
+        }
+    }
+
+    //create marker size function
+    function radiusSize(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+
+        return magnitude * 5;
+    }
+
+    
+
+    // make some circles
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: geojsonStyle,
+        onEachFeature (feature, layer) {
+            layer.bindPopup(`Magnitude ${feature.properties.mag}, Location: ${feature.properties.place}`);
+        }
+    }).addTo(myMap);
+});
+
+
