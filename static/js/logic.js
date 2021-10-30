0b0775b7b8ba13fd9e0b13dfbdcb5da638757c74
sshaughnessy <s.shaughnessy.311@gmail.com>
2021-10-30 17:50:58 -0500

    for submit

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,33 +18,37 @@ var myMap = L.map("map", {
  // read in json and add earthquake markers to the map
 d3.json(url).then(function(data) {
     // create marker style for L.geoJSON
+    
+  /*  
     function geojsonStyle(feature) {
         return { 
             opacity: 1,
             fillOpacity: 1,
             fillColor: colorScale(feature.geometry.coordinates[2]),
-            color: "#000000",
+            //color: "#000000",
             radius: radiusSize(feature.properties.mag),
             stroke: true,
             weight: 0.5
         };
     }
+    */
     
     // create color scale function
     function colorScale(depth) {
         switch(depth) {
-            case depth > -10 && depth <=10:
-            return "#00ff00";
-            case depth > 10 && depth <=30:
-                return "#ccff66";
-            case depth > 30 && depth<=50:
-                return "#ffcc00";
-            case depth > 50 && depth<=70:
-                return "#ff9900";
-            case depth > 70 && depth<=90:
-                return "#ff6633";
             case depth > 90:
-                return "#ff3300";
+                return "red";
+            case depth > 70:
+                return "tomato"
+            case depth > 50:
+                return "orange";
+            case depth > 30:
+                return "gold";
+            case depth > 10:
+                return "yellow";
+            default:
+                return "yellowgreen";
+            
         }
     }
 
@@ -62,13 +66,41 @@ d3.json(url).then(function(data) {
     // make some circles
     L.geoJSON(data, {
         pointToLayer: function(feature, latlng) {
-            return L.circleMarker(latlng);
+            return L.circleMarker(latlng,
+                {
+                    radius: radiusSize(feature.properties.mag),
+                    fillOpacity: 0.7,
+                    fillColor: colorScale(feature.geometry.coordinates[2]),
+                    color: "black",
+                    stroke: true,
+                    weight: 0.5
+                }
+                );
         },
-        style: geojsonStyle,
         onEachFeature (feature, layer) {
             layer.bindPopup(`Magnitude ${feature.properties.mag}, Location: ${feature.properties.place}`);
         }
     }).addTo(myMap);
+
+    // legend
+    var legend = L.control({ 
+        position: "bottomright"
+    });
+
+    legend.onAdd = function () {
+        var div = L.DomUtil.create("div", "info legend");
+        var depth = [-10, 10, 30, 50, 70, 90];
+
+        div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+        for (var i =0; depth.length; i++) {
+            div.innerHTML += 
+            '<i style="background: ' + colorScale(depth[i] + 1) + '"></i> ' +
+                    depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+                  
+                return div;
+        }
+    }
+    legend.addTo(myMap);
 });
 
 
